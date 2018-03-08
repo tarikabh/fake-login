2f5f55b11958ad388a0ca9da7ed3d849e224e1d3
Tarik Demirović <tarik.demirovic@atlantbh.com>
2018-03-08 15:32:52 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+let app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(req, res) {
+  console.log('Serving authentication page...')
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.post('/', function(req, res) {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  if(username && password) {
+    if (username === 'abh' && password === 'abhpw') {
+      res.status(200);
+      return res.send("<script>alert('Success!');</script>");
+    } else {
+      res.status(400);
+      return res.send("<script>alert('Wrong credentials!');</script>");
+    }
+  } else {
+    res.status(400);
+    res.send("<script>alert('Please enter username and password!');</script>");
+  }
+});
+
+let port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`App listening on port ${port}!`))
